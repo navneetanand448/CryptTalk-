e9a836dfba56723755b1eba940d90d032e1b89c7
rahulpatel0408 <147559135+rahulpatel0408@users.noreply.github.com>
2025-09-09 04:47:44 +0530

    logging

--- src/socket.jsx
+++ src/socket.jsx
@@ -6,7 +6,7 @@ const SocketContext = createContext()
 
 const getSocket = () => useContext(SocketContext)
 const SocketProvider = ({children})=>{
-    const socket = useMemo(()=>io(server,{withCredentials:true}), []);
+    const socket = useMemo(()=>io(server,{ transports: ["polling"], withCredentials:true}), []);
     return (
         <SocketContext.Provider value={socket}>
             {children}
